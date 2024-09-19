cd01a8788304d511bfa5512aa8e31c8e439fc316
PramodkrOfficial <pramodkumarrupauliya@gmail.com>
2024-09-19 18:25:52 +0530

    new experiment is going on

--- src/components/Contact1.jsx
+++ src/components/Contact1.jsx
@@ -3,7 +3,6 @@ import { useForm } from 'react-hook-form';
 
 
 function Contact1() {
-    render2();
     // <form name="contact" method="POST" netlify>
     //     <p>
     //         <label>Your Name: <input type="text" name="name" /></label>
@@ -25,64 +24,35 @@ function Contact1() {
     //     </p>
     // </form>
 
-    const encode = (data) => {
-        return Object.keys(data)
-            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-            .join("&");
-    }
 
-    class ContactForm extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = { name: "", email: "", message: "" };
-        }
+    return (
+        <>
+            <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+                <input type="text" name="name" />
+                <input type="email" name="email" />
+                <textarea name="message"></textarea>
+            </form>
 
-        /* Here’s the juicy bit for posting the form submission */
-
-        handleSubmit = e => {
-            fetch("/", {
-                method: "POST",
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: encode({ "form-name": "contact", ...this.state })
-            })
-                .then(() => alert("Success!"))
-                .catch(error => alert(error));
-
-            e.preventDefault();
-        };
-
-        handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-        render2() {
-            const { name, email, message } = this.state;
-            return (
-                <form onSubmit={this.handleSubmit}>
-                    <p>
-                        <label>
-                            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            Message: <textarea name="message" value={message} onChange={this.handleChange} />
-                        </label>
-                    </p>
-                    <p>
-                        <button type="submit">Send</button>
-                    </p>
-                </form>
-            );
-        }
-    }
-
-
-
+            <div id="root"></div>
+            <form name="contact" method="post">
+                <input type="hidden" name="form-name" value="contact" />
+                <p>
+                    <label>Your Name: <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Your Email: <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        </>
+    )
 }
+
 
 
 
--- src/components/Contact2.jsx
+++ src/components/Contact2.jsx
@@ -1,93 +1,93 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+// import React from 'react';
+// import { useForm } from 'react-hook-form';
 
 
-function Contact() {
-    // <form name="contact" method="POST" netlify>
-    //     <p>
-    //         <label>Your Name: <input type="text" name="name" /></label>
-    //     </p>
-    //     <p>
-    //         <label>Your Email: <input type="email" name="email" /></label>
-    //     </p>
-    //     <p>
-    //         <label>Your Role: <select name="role[]" multiple>
-    //             <option value="leader">Leader</option>
-    //             <option value="follower">Follower</option>
-    //         </select></label>
-    //     </p>
-    //     <p>
-    //         <label>Message: <textarea name="message"></textarea></label>
-    //     </p>
-    //     <p>
-    //         <button type="submit">Send</button>
-    //     </p>
-    // </form>
+// function Contact() {
+//     // <form name="contact" method="POST" netlify>
+//     //     <p>
+//     //         <label>Your Name: <input type="text" name="name" /></label>
+//     //     </p>
+//     //     <p>
+//     //         <label>Your Email: <input type="email" name="email" /></label>
+//     //     </p>
+//     //     <p>
+//     //         <label>Your Role: <select name="role[]" multiple>
+//     //             <option value="leader">Leader</option>
+//     //             <option value="follower">Follower</option>
+//     //         </select></label>
+//     //     </p>
+//     //     <p>
+//     //         <label>Message: <textarea name="message"></textarea></label>
+//     //     </p>
+//     //     <p>
+//     //         <button type="submit">Send</button>
+//     //     </p>
+//     // </form>
 
 
-    // const {
-    //     register,
-    //     handleSubmit,
-    //     watch,
-    //     formState: { errors },
-    // } = useForm()
-    // const onSubmit = (data) => console.log(data)
+//     // const {
+//     //     register,
+//     //     handleSubmit,
+//     //     watch,
+//     //     formState: { errors },
+//     // } = useForm()
+//     // const onSubmit = (data) => console.log(data)
 
 
 
-    const { name, email, message } = this.state;
-    class ContactForm extends React.Component {
-        constructor(props) {
-            super(props);
-        }
+//     const { name, email, message } = this.state;
+//     class ContactForm extends React.Component {
+//         constructor(props) {
+//             super(props);
+//         }
         
-        /* Here’s the juicy bit for posting the form submission */
+//         /* Here’s the juicy bit for posting the form submission */
         
-        handleSubmit = e => {
-            fetch("/", {
-                method: "POST",
-                headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                body: encode({ "form-name": "contact", ...this.state })
-            })
-                .then(() => alert("Success!"))
-                .catch(error => alert(error));
+//         handleSubmit = e => {
+//             fetch("/", {
+//                 method: "POST",
+//                 headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//                 body: encode({ "form-name": "contact", ...this.state })
+//             })
+//                 .then(() => alert("Success!"))
+//                 .catch(error => alert(error));
 
-            e.preventDefault();
-        };
+//             e.preventDefault();
+//         };
         
-        handleChange = e => this.setState({ [e.target.name]: e.target.value });
+//         handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
         
-        this.state = { name: "", email: "", message: "" };
-    return (
-        <>
-            <form onSubmit={this.handleSubmit}>
-                    <p>
-                        <label>
-                            Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            Message: <textarea name="message" value={message} onChange={this.handleChange} />
-                        </label>
-                    </p>
-                    <p>
-                        <button type="submit">Send</button>
-                    </p>
-                </form>
-        </>
-    )
-}
+//         this.state = { name: "", email: "", message: "" };
+//     return (
+//         <>
+//             <form onSubmit={this.handleSubmit}>
+//                     <p>
+//                         <label>
+//                             Your Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
+//                         </label>
+//                     </p>
+//                     <p>
+//                         <label>
+//                             Your Email: <input type="email" name="email" value={email} onChange={this.handleChange} />
+//                         </label>
+//                     </p>
+//                     <p>
+//                         <label>
+//                             Message: <textarea name="message" value={message} onChange={this.handleChange} />
+//                         </label>
+//                     </p>
+//                     <p>
+//                         <button type="submit">Send</button>
+//                     </p>
+//                 </form>
+//         </>
+//     )
+// }
 
 
 
 
 
 
-export default Contact;
+// export default Contact;
