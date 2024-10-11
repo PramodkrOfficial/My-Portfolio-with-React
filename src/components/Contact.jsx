12aa53475afa55f8a60283761733bd27f1719ada
PramodkrOfficial <pramodkumarrupauliya@gmail.com>
2024-10-11 17:32:40 +0530

    new experiment is going on

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,17 +34,12 @@ function Contact() {
     const onSubmit = (data) => console.log(data)
     return (
         <>
-<<<<<<< HEAD
             <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-16'>
-=======
-            <div name='Contact' className='max-w-screen-2xl container mx-auto px-4 md:px-20 my-16'>
->>>>>>> 2dbd724a0cdfc448eae8d407ad8668fcf9522a70
                 <h1 className='text-3xl font-bold mb-4'>Contact me</h1>
                 <span>Please fill out the form below to contact me</span>
                 <div className='flex flex-col items-center justify-center mt-5'>
                     <form
-<<<<<<< HEAD
-                        name="contact" netlify netlify-honeypot="bot-field"
+                        name="contact" netlify hidden
                         action="post"
                         onSubmit={handleSubmit(onSubmit)}
                         className='bg-slate-200 w-96 px-8 py-6 rounded-xl'>
@@ -72,35 +67,6 @@ function Contact() {
                                 {...register("message", { required: true })}
                                 className='outline-none border-2 border-gray-300  rounded-xl h-20 py-2 px-3 text-gray-700'
                                 id='message'
-=======
-                    netlify
-                        onSubmit={handleSubmit(onSubmit)}
-                        action=""
-                        className='bg-slate-200 w-96 px-8 py-6 rounded-xl'>
-                        <h1 className='text-xl font-semibold mb-4'>Send Your Message</h1>
-                        <div className='flex flex-col mb-4'>
-                            <label className='block text-gray-700' htmlFor="name">Full Name</label>
-                            <input
-                                {...register("name", { required: true })}
-                                className='shadow appearance-none border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id='name' name='name' type="text" placeholder='Enter your full name' />
-                            {errors.name && <span>This field is required</span>}
-                        </div>
-                        <div className='flex flex-col mb-4'>
-                            <label className='block text-gray-700' htmlFor="name">Email Address</label>
-                            <input
-                                {...register("email", { required: true })}
-                                className='shadow appearance-none border rounded-lg py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' id='name' name='email' type="text" placeholder='Enter your email' />
-                            {errors.email && <span>This field is required</span>}
-                        </div>
-    
-                        <div className='flex flex-col mb-4'>
-                            <label className='block text-gray-700' htmlFor="name">Message</label>
-                            <textarea
-                                {...register("message", { required: true })}
-                                className='outline-none border-2 border-gray-300  rounded-xl h-20 py-2 px-3 text-gray-700'
-                                id='message'
-                                name='message'
->>>>>>> 2dbd724a0cdfc448eae8d407ad8668fcf9522a70
                                 rows='4'
                                 placeholder='Type your message here'
                             >
