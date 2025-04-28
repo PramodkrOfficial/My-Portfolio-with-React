3ca91bc6129a7d10dcd8608d096aff02adb78f9d
PramodkrOfficial <pramodkumarrupauliya@gmail.com>
2025-04-29 01:04:26 +0530

    initial commit

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -3,13 +3,15 @@ import React, { useEffect } from 'react';
 const WelcomeAlert = () => {
     useEffect(() => {
         alert("Welcome to you my website");
-    }, []); 
+    }, []);
 
     return (
-        <div>
-            <h1>Welcome to you my website</h1>
-            {/* Other content can go here */}
-        </div>
+        <>
+            <div>
+                <h1>Welcome to you my website</h1>
+                {/* Other content can go here */}
+            </div>
+        </>
     );
 };
 
